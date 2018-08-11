bc5cf17334fa35408ffae37e70ec9cd65fb727d6
田原 一樹 <ssp0727.lnc@gmail.com>
2018-08-11 11:41:25 +0900

    add file

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <app-quote
+      v-for="(quote, index) in quotes"
+      :key="index"
+      @click.native="deleteQuote(index)">
+      {{ quote }}
+    </app-quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue';
+  export default {
+    props: {
+      quotes: Array
+    },
+    components: {
+      appQuote: Quote
+    },
+    methods: {
+      deleteQuote(index){
+        this.$emit('quoteDeleted',index);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
